14162a8cba9cbbc74a4788b09b17267b37827aab
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2025-10-15 18:16:18 +0600

    modified on web.php file

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    Route::resource('posts', PostController::class);
 });
-Route::resource('posts',PostController::class)->middleware('auth');
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
